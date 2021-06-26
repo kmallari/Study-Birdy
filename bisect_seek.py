0bad5e2a28db7c7c329c0bad5c21efce4628ae08
Kevin Mallari <60468262+kmallari@users.noreply.github.com>
2021-06-26 06:45:45 +0000

    Adds bisect search for more optimizing searching.

---
+++ bisect_seek.py
@@ -0,0 +1,61 @@
+import csv
+import os
+
+def get_line_at_pos(fin, pos):
+    fin.seek(pos)
+    skip = fin.readline()
+    npos = fin.tell()
+    assert pos + len(skip) == npos
+    line = fin.readline()
+    return npos, line
+
+def bisect_seek(fname, field_func, field_val):
+    size = os.path.getsize(fname)
+    minpos, maxpos, cur = 0, size, int(size / 2)
+
+    with open(fname) as fin:
+        prev_pos = -1
+        while True:  # find first id smaller than the one we search
+            realpos, line = get_line_at_pos(fin, cur)
+            val = field_func(line)
+            if val >= field_val:
+                state = ">"
+                maxpos = cur
+                cur = int((minpos + cur) / 2)
+            else:
+                state = "<"
+                minpos = cur
+                cur = int((cur + maxpos) / 2)
+            if prev_pos == cur:
+                break
+            prev_pos = cur
+    return realpos
+
+
+def getser(line):
+    return line.split(",")[0]
+
+
+def find_subject(filename, class_code, section):
+    found = False
+    # find position from which we should start searching
+    pos = bisect_seek(filename, getser, class_code)
+    with open(filename, 'r', encoding='utf_8_sig') as csvfile:
+        csvfile.seek(pos)
+        reader = csv.reader(csvfile, delimiter=',')
+        try:
+            for row in reader:
+                if row[0] == class_code and row[1] == section:
+                    found = True
+                    break
+                elif row[0] > class_code:
+                    # as file is sorted we know we can abort now
+                    break
+        except Exception as e:
+            print(e)
+    if found:
+        print("good row", row)
+        return {'result': True, 'message': f'Class found'}
+    else:
+        print("bad row", row)
+        return {'result': False, 'message': f'Class not found in Database'}
--- main.py
+++ main.py
@@ -2,10 +2,28 @@ from discord.ext import commands
 import discord
 import os
 from scraper import update_database
+from bisect_seek import find_subject
+from replit import db # allows access to replit database
 
 # client = discord.Client()
 
 bot = commands.Bot(command_prefix="~")
+
+def add_subj_to_db(class_code, section):
+    code_sec_str = f'{class_code} {section}'
+    if code_sec_str in db.keys():
+        return
+    subj_info = find_subject('subjects.csv', class_code, section)
+    db[code_sec_str] = subj_info
+    print(db[code_sec_str])
+
+def add_to_role(id, class_code, section):
+    add_subj_to_db(class_code, section)
+    # ---- pseudocode -----
+    # check if subject role exists. if not, then call add_subj_to_db
+        # now, make the role
+    # add user to the role
+    # ---- end of pseudocode ----
 
 @bot.event
 async def on_ready():
@@ -18,14 +36,19 @@ async def on_ready():
 #     await ctx.send(arg)
 
 @bot.command()
-async def update(arg):
-    await arg.channel.send('Updating the database. This will take around three (3) minutes to finish. You will be pinged after the database is updated. In the meantime, the bot will be unusable.')
+async def update(ctx):
+    await ctx.channel.send('Updating the database. This will take around three (3) minutes to finish. You will be pinged after the database is updated. In the meantime, the bot will be unusable.')
     await update_database()
-    await arg.channel.send(f'Database is updated. <@{arg.author.id}>')
+    await ctx.channel.send(f'Database is updated. <@{ctx.author.id}>')
 
 @bot.command()
 async def add(ctx, class_code, section):
     await ctx.channel.send(f'The class code you entered is: {class_code}')
     await ctx.channel.send(f'The section you entered is: {section}')
+    add_to_role(ctx.message.author.id, class_code, section)
+
+# @bot.command()
+# async def test(ctx,):
+#     add_to_role(ctx.message.author.id, 'PHILO132.2i', 'A')
             
 bot.run(os.getenv('TOKEN'))
